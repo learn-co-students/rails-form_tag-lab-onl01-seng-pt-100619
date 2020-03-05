6979b91cf1fcdc59f6b17ad053c4ab0976e871cb
github username <github email address>
2020-03-05 17:46:09 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -13,9 +15,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to new_student_path(@student)
-    
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
   end
 
 
