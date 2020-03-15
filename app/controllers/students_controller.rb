33b7e7a60fccdca5033b576326b28d514dd13059
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-03-14 21:05:30 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
+  end
+
+  #the new action is not required as a method in the student controller
+  #the new view is rendered without the need of a method in the student controller
+  #the '/student/new' action on the new student form uses the create action
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
+  post '/students/new', to: 'students#create'
 end
+
+#the create action does not render a view but it creates a student object 
+#and it does it on the url '/students/new'
+#the create action only executes the block of code in the student controller method
+#the url/route helper new_student_path = '/student/new' and its used on the form that triggers the create action
+#the url/route helper student_path(@students) = '/student/id:'  is used to redirect from the create action
