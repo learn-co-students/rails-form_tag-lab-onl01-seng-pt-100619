7b6fb233ec6ac1ccb958a57f5ac77c134f452816
wendyv510 <wendyv510@hotmail.com>
2020-03-28 14:23:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,25 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new 
+    @student = Student.new
+  end 
+
+  def create 
+    @student = Student.create(student_params)  
+    @student.save 
+      redirect_to student_path(@student) 
+  end 
+
   def show
     @student = Student.find(params[:id])
   end
+
+    private 
+
+  def student_params 
+    params.require(:student).permit(:first_name, :last_name) 
+  end 
+
 
 end
